984f61bc0bb0724386f7845eb5821427e5e0f2fa
Ian Byerrum <ibyerrum@ackerman.k12.ca.us>
2016-12-15 13:55:23 -0800

    Update Byerrum1.c

--- Byerrum1.c
+++ Byerrum1.c
@@ -1,43 +1,48 @@
 /*
-  Blank Simple Project.c
+  Byerrum1.c
+  Lesson 1 for Computer Science
+  
+  Reference:
   http://learn.parallax.com/propeller-c-tutorials 
 */
+
 #include "simpletools.h"                      // Include simple tools
 
 int main()                                    // Main function
 {
-  // Add startup code here.
+  // Shows the user a simple introduction
   print("Hello world!\n");
   print("Welcome to my favorite school\n");
   print("Bowman Charter School\n");
 
-  int a = 0;
-  int b = 0;
-  int c;
-  char repeat = 'y';
+  // Initialize varibles for use in this program
+  int a = 0;                                  // First number to be entered by user
+  int b = 0;                                  // Second number to be entered by user
+  int c;                                      // Calculated sum of the two numbers
+  char repeat = 'y';                          // Will tell when the user wishes to exit program
   
-  while(repeat == 'y')
+  while(repeat == 'y')                        // Repeats until the user wishes to exit
   {
     print("Enter a number: ");
     scan("%d\n", &a);
     print("Enter a second number: ");
-   scan("%d\n", &b);
+    scan("%d\n", &b);
   
-   c = a + b;
+    c = a + b;                                 // Adds the two entered numbers together
   
-   print("The value of c is %d\n", c);
+    print("The value of c is %d\n", c);
                 
-   if(c > 0)
-   {
-     print("The value of c is positive");
-   }
+    if(c > 0)                                  // Runs only if the sum is positive
+    {
+      print("The value of c is positive");
+    }
   
-   if(c < 0)
-   {
-     print("The value of c is negative");
-   }          
+    if(c < 0)                                  // Runs only if the sum is negative
+    {
+      print("The value of c is negative");
+    }          
    
-   print("\nWould you like to perform another calculation? (y/n)");
-   scan("%c\n", &repeat);
-  }  
+    print("\nWould you like to perform another calculation? (y/n)");  // Loop will repeat only if user enters "y"
+    scan("%c\n", &repeat);
+   }  
 }
